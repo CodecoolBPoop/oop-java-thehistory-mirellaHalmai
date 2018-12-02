e32a9e9ea6cc476cbe850176fe07152a75213e3a
Halmai Mirella <halmaimirella@gmail.com>
2018-12-02 14:06:16 +0100

    Replace more words using listIterator ready

--- src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
@@ -49,50 +49,39 @@ public class TheHistoryLinkedList implements TheHistory {
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
-        LinkedList<String> newList = new LinkedList<>();
-        ListIterator<String> words = wordsLinkedList.listIterator();
         LinkedList<String> fromLinkedList = new LinkedList<>(Arrays.asList(fromWords));
+        LinkedList<String> toLinkedList = new LinkedList<>(Arrays.asList(toWords));
+        ListIterator<String> words = wordsLinkedList.listIterator();
         String firstFromWord = fromLinkedList.removeFirst();
         int fromLength = fromWords.length;
-        LinkedList<String> toLinkedList = new LinkedList<>(Arrays.asList(toWords));
 
-        while (size() > 0) {
-            if (size() < fromLength) {
-                newList.addAll(wordsLinkedList);
-                break;
-            } else {
-                String nextWord = wordsLinkedList.removeFirst();
-                if (!nextWord.equals(firstFromWord)) {
-                    newList.add(nextWord);
-                } else {
-                    LinkedList<String> temporary = new LinkedList<>();
-                    temporary.add(nextWord);
-                    boolean allWordsEqual = true;
-                    for (String fromWord : fromLinkedList) {
-                        nextWord = wordsLinkedList.removeFirst();
-                        if (!nextWord.equals(fromWord)) {
-                            newList.addAll(temporary);
-                            wordsLinkedList.addFirst(nextWord);
-                            allWordsEqual = false;
-                            break;
-                        }
-                        temporary.add(nextWord);
+        while (words.nextIndex() <= size() - fromLength) {
+            int nextElementIndex = words.nextIndex();
+            String nextWord = words.next();
+            if (nextWord.equals(firstFromWord)) {
+                boolean allWordsEqual = true;
+                for (String fromWord : fromLinkedList) {
+                    String followingWordToCheck = words.next();
+                    if (!followingWordToCheck.equals(fromWord)) {
+                        allWordsEqual = false;
+                        break;
                     }
-                    if (allWordsEqual) {
-                        newList.addAll(toLinkedList);
+                }
+                if (allWordsEqual) {
+                    while (words.previousIndex() > nextElementIndex - 1) {
+                        words.previous();
+                        words.remove();
+                    }
+                    for (String toWord : toLinkedList) {
+                        words.add(toWord);
+                    }
+                } else {
+                    while (words.previousIndex() > nextElementIndex) {
+                        words.previous();
                     }
                 }
             }
         }
-        wordsLinkedList = newList;
-
-        //while (words.hasNext()) {
-        //    if (words.nextIndex() > size() - fromLength) {
-        //        words.forEachRemaining(word -> newList.add(word));
-        //    } else if (!words.next().equals(firstFromWord)) {
-
-        //    }
-        //}
     }
 
     @Override
@@ -103,22 +92,5 @@ public class TheHistoryLinkedList implements TheHistory {
         }
         if (sb.length() > 0) sb.deleteCharAt(sb.length() - 1); // last space char
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        String string = "Test this and this and this also this if you can";
-        String[] from = {"that", "and"};
-        String[] to = {"not", "this"};
-        TheHistoryArray history = new TheHistoryArray();
-        history.add(string);
-        System.out.println(history.toString());
-        history.replaceOneWord("this", "that");
-        System.out.println(history.toString());
-        history.replaceMoreWords(from, to);
-        System.out.println(history.toString());
-        history.removeWord("that");
-        System.out.println(history.toString());
-        history.clear();
-        System.out.println(history.toString());
     }
 }
